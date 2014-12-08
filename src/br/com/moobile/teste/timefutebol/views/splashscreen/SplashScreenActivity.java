d337365f4ee4e4f8a4a7b502a98ea3f9c344c30c
Estevão Andrade <estevao@uololou.com.br>
2014-12-08 12:57:33 -0200

    Acertando registro no servidor

--- src/br/com/moobile/teste/timefutebol/views/splashscreen/SplashScreenActivity.java
+++ src/br/com/moobile/teste/timefutebol/views/splashscreen/SplashScreenActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.Settings.Secure;
 import br.com.moobile.teste.timefutebol.R;
 import br.com.moobile.teste.timefutebol.utils.data.PreferencesWrapper;
 import br.com.moobile.teste.timefutebol.utils.rest.RestClient;
@@ -16,16 +16,24 @@ public class SplashScreenActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-		new SplashScreenTask().execute();
+		
+		String android_id = Secure.getString(getContentResolver(),Secure.ANDROID_ID);
+		new SplashScreenTask(android_id).execute();
 	}
 
 	private class SplashScreenTask extends AsyncTask<Void, Void, Void> {
 
+		private String android_id;
+		
+		public SplashScreenTask(String android_id) {
+			super();
+			this.android_id = android_id;
+		}
 		@Override
 		protected Void doInBackground(Void... params) {
 			try {
 				Thread.sleep(1000);
-				String token = RestClient.getApiService().register("testeste"); 
+				String token = RestClient.getApiService().register(android_id); 
 				PreferencesWrapper.put("token", token);
 			} catch (InterruptedException e) { 
 				e.printStackTrace();
